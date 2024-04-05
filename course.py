904eda7c946bd3395204036be938d65af56bd654
indranil <indranilsaha046@gmail.com>
2024-04-05 21:56:50 +0530

    Add course.py and payment.py files

---
+++ course.py
@@ -0,0 +1,9 @@
+from payment import payment2
+
+def course():
+    print("This is my course function")
+    payment2()  # Call payment2 function from payment.py
+
+if __name__ == "__main__":
+    #print("Running course.py directly")
+    course()  # Call course function
---
+++ payment.py
@@ -0,0 +1,8 @@
+
+
+def payment2():
+    print("This is my payment2 function")
+
+if __name__ == "__main__":
+    print("Running payment.py directly")
+    
